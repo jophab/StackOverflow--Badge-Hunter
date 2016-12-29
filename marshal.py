37f93592554f0a11bc6ac759eb3ec76425a613e2
JOBIN PHILIP ABRAHAM <jpa895@gmail.com>
2016-12-29 08:59:09 +0530

    Create marshal.py

---
+++ marshal.py
@@ -0,0 +1,31 @@
+import json
+import re
+import webbrowser
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+result = []
+def find_bad_qn(a):
+	url="https://stackoverflow.com/questions?page="+str(a)+"&sort=newest"
+	html = urlopen(url)
+	bsObj = BeautifulSoup(html,"html5lib")
+	que= bsObj.find_all("div", class_="question-summary")
+	for div in que:
+		vote=int(div.find('span').text.split(' ')[0])
+		link=div.a.get('href')
+		name=div.a.text
+		if(vote<-2 and not bool(re.search('(\[on hold\]|\[duplicate\]|\[closed\])$', name))):
+		    result.append({'link': link, 'vote': vote,'name':name})
+
+for i in range(45,65):
+	find_bad_qn(i)
+
+for qn in result:
+	qn['link']="https://stackoverflow.com"+qn['link']	
+
+result = json.dumps({'result': sorted(result, key=lambda x: x['vote'], reverse=False)})	
+result=json.loads(result)
+for qn in result['result']:
+	print(qn['link']," Votes :", qn['vote'])
+	url=qn['link']
+	webbrowser.open(url, new=0, autoraise=True)
+exit(0)
